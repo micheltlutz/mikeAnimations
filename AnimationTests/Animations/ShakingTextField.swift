f36b1bb85d6e9b7d05ce20a63ea6a34aad2f2eb8
Michel Anderson Lutz Teixeira <michel.teixeira@pc28.thema.local>
2018-04-11 17:02:03 -0300

    first commit

---
+++ AnimationTests/Animations/ShakingTextField.swift
@@ -0,0 +1,21 @@
+//
+//  ShakingTextField.swift
+//  AnimationTests
+//
+//  Created by Michel Anderson Lutz Teixeira on 11/04/2018.
+//  Copyright © 2018 Michel Anderson Lutz Teixeira. All rights reserved.
+//
+
+import UIKit
+
+class ShakingTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ AnimationTests/Views/CA_ShakingAndPulseViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CA_ShackAndPulseViewController.swift
+//  AnimationTests
+//
+//  Created by Michel Anderson Lutz Teixeira on 11/04/2018.
+//  Copyright © 2018 Michel Anderson Lutz Teixeira. All rights reserved.
+//
+
+import UIKit
+
+class CA_ShackAndPulseViewController: UIViewController {
+
+    @IBOutlet weak var tfShaking: ShakingTextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
